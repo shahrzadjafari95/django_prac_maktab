b9f5ac4e75430338a8dc92157856ea684d997b5b
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-08 00:34:09 +0330

    create home page
    create home page without addressing the urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('website/', include('website.urls'))
+    path('', include('website.urls')),
 
 ]
