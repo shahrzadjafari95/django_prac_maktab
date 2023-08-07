62a9f40b80cd47f095b19526e9f8d4e430b41780
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-08 00:22:14 +0330

    define 3 urls
    define home and about and contact urls in first app

--- mysite/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-
-
-def http_test(request):
-    return HttpResponse('<h1> Hello this is test site.</h1>')
-
-
-def json_test(request):
-    return JsonResponse({'name': 'shahrzad'})
--- website/views.py
+++ website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
+
+
+def home_view(request):
+    return HttpResponse('<h1> Hello this is Home Page.</h1>')
+
+
+def about_view(request):
+    return HttpResponse('<h1> Hello this is About Page.</h1>')
+
+
+def contact_view(request):
+    return HttpResponse('<h1> Hello this is Contact Page.</h1>')
