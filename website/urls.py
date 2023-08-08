ac2c7eb74357be8b570ecf391e2564cbdae0aad7
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-08 20:02:32 +0330

    add template file
    add template file in base directory and move html files in templates file

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from website.views import *
 
-urlpatterns = [path('home/', home_view),
+urlpatterns = [path('', home_view),
                path('about', about_view),
                path('contact', contact_view),
                ]
