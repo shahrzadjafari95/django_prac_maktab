d43cedc6d8a23913be3baac9e1805357eadbf34b
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-08 18:02:18 +0330

    change home urls
    change home urls for test and check it

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from website.views import *
 
-urlpatterns = [path('', home_view),
+urlpatterns = [path('home/', home_view),
                path('about', about_view),
                path('contact', contact_view),
                ]
