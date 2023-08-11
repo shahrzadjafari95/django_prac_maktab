b59ad9ebdbc616f8344699c003d6bfe75485db3a
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-12 00:13:55 +0330

    add blog app
    + add blog app
    + define urls.py in blog
    + define views.py in blog'
    + design and set blog home and details

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def blog_view(request):
+    return render(request, 'blog/blog-home.html')
+
+
+def single_view(request):
+    return render(request, 'blog/blog-single.html')
