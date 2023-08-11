64c99a2833f4a9d5278e70df9188c2705beaec38
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-11 21:37:29 +0330

    design template
    create base.html and use inheritance of base.html

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
 
-urlpatterns = [path('', home_view),
-               path('about/', about_view),
-               path('contact/', contact_view),
+urlpatterns = [path('', home_view, name='index'),
+               path('about/', about_view, name='about'),
+               path('contact/', contact_view, name='contact'),
                ]
