c02bf797728251508ee6916e999b463035a30898
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-08-19 21:31:55 +0330

    use url input
    use url input

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,9 @@ from blog.views import *
 app_name = 'blog'
 
 urlpatterns = [path('', blog_view, name='index'),
-               path('single/', single_view, name='single'),
-               path('test/', test, name='test'),
+               path('single-<int:pid>', single_view, name='single'),
+               # path('name/<str:name>/lastname/<str:family>/age/<str:age>/', test, name='test'),
+               # path('post-<int:pid>/', test2, name='test'),
+               # path('post--<int:pid>/', test3, name='test'),
 
                ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from datetime import datetime
 
 
 # Create your views here.
 
 
+# def blog_view(request):
+#     posts = Post.objects.filter(status=1)
+#     context = {'posts': posts}
+#     return render(request, 'blog/blog-home.html', context)
+
+
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
-    context = {'posts':posts}
+    current_time = datetime.now()
+    posts = Post.objects.filter(published_date__lte=current_time)
+    context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 
-def single_view(request):
-    return render(request, 'blog/blog-single.html')
+def single_view(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    if post:
+        post.counted_view += 1
+        post.save()
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
+
+## for test
+# def test(request, name, family, age):
+#     context = {'name': name, 'family': family, 'age': age}
+#     return render(request, 'test.html', context)
 
 
-def test(request):
-    posts = Post.objects.filter(status=0)
-    context = {'posts': posts}
-    return render(request, 'test.html', context)
+# def test2(request, pid):
+#     context = {'pid': pid}
+#     return render(request, 'test.html', context)
+
+
+# def test3(request, pid):
+#     post = get_object_or_404(Post, pk=pid)
+#     context = {'post': post}
+#     return render(request, 'test.html', context)
