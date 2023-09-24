0b8fc404d697b1cfdb2853e42c6786fb79d22478
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-09-24 11:05:23 +0330

    define signup view
    define signup view

--- accounts/views.py
+++ accounts/views.py
@@ -7,3 +7,6 @@ from django.shortcuts import render
 def login_view(request):
     return render(request, 'accounts/login.html')
 
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
