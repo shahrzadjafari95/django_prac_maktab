1dfc9dca0742dd37c8a1e688f88ea1bc90d49148
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-09-24 11:05:43 +0330

    add logout view
    define logout view

--- accounts/views.py
+++ accounts/views.py
@@ -8,5 +8,9 @@ def login_view(request):
     return render(request, 'accounts/login.html')
 
 
+def logout_view(request):
+    pass
+
+
 def signup_view(request):
     return render(request, 'accounts/signup.html')
