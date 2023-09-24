cb9bc3f414b9df107fba13f9a969b5c2f59d8e9b
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-09-25 00:23:01 +0330

    define login view
    define login view  for login form and check authentication user input

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'user is authenticated as  {request.user.username}'
-    else:
-        msg = 'user is not authenticated'
-    return render(request, 'accounts/login.html', {'msg': msg})
+    if request.method == "POST":
+        username = request.POST["username"]  # Something that user enter in login form
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)  # Checks in the db whether it exists or not
+        if user is not None:  # if exists
+            login(request, user)
+            return redirect('/blog/category/entertainment/')  # redirect to home page
+    return render(request, 'accounts/login.html')
 
 
 def logout_view(request):
