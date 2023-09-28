9bc1362346eee41f5c6053c2cfdc1754354178b2
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-09-28 21:58:34 +0330

    create custom user model in account app
    create custom user model in account app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
