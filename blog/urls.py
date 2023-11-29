a2a1bdac65be9cf32bc8a0bf43904f5355e35e2d
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2023-11-30 01:42:06 +0330

    change single post url
    change url for single view

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from blog.views import *
 app_name = 'blog'
 
 urlpatterns = [path('', blog_view, name='index'),
-               path('post-<int:pid>/', single_view, name='single'),
-               path('name/<str:name>/', test_view, name='test'),
+               path('post<int:pid>/', single_view, name='single'),
+               path('test<str:name>/', test_view, name='test'),
                path('category/<str:cat_name>/', blog_view, name='category'),
                path('tag/<str:tag_name>/', blog_view, name='tag'),
                path('author/<str:author_username>/', blog_view, name='author'),
