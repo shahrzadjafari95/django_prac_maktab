222e96e3d09f2c3514f6ca18648ea59db45375bc
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2024-01-04 22:33:08 +0330

    change function name for test url
    change function name for test url and connect test url to test_for_contact_form_with_modelform function in view

--- website/urls.py
+++ website/urls.py
@@ -7,6 +7,6 @@ app_name = 'website'
 urlpatterns = [path('', home_view, name='index'),
                path('about/', about_view, name='about'),
                path('contact/', contact_view, name='contact'),
-               path('test/', test_view, name='test'),
+               path('test/', test_for_contact_form_with_modelform, name='test'),
                path('newsletter/', newsletter_view, name='newsletter'),
                ]
