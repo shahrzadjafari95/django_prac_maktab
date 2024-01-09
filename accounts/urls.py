f615fd319f1ce640d4d0a9161a0fa3bdd17654c1
shahrzadjafari95 <jafari.shahrzad74@gmail.com>
2024-01-10 00:47:08 +0330

    edit comment
    edit comment for three urls and explain about this urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    # login url
-    path('login/', views.login_view, name='login'),
-    # logout url
-    path('logout/', views.logout_view, name='logout'),
-    # registration url
-    path('signup/', views.signup_view, name='signup'),
+
+    path('login/', views.login_view, name='login'),  # login url
+
+    path('logout/', views.logout_view, name='logout'),  # logout url
+
+    path('signup/', views.signup_view, name='signup'),  # registration url
 ]
