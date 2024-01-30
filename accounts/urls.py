438e5dd1172f43310e87ed23150d5b4c7fcbb97c
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-01-30 14:03:07 +0330

    create password_reset_done url
    use  password_reset_done view for show page if user want to change password and show password_reset_done.html.
    in this step we sent an email for reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
     PasswordResetView,
+    PasswordResetDoneView,
 from django.urls import path
 
 from . import views
@@ -15,4 +16,8 @@ urlpatterns = [
 
     path('password-reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
          name='password-reset'),
+
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+
 ]
