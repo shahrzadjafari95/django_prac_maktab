b1e830de4acce8d634e9c8430840373dd1bf1640
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-07-27 14:32:15 +0330

    set media url in setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = 'statics/'
 MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
